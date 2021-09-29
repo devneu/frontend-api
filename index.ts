4bc6d8a8a7019366e8fc2efa1701731480fef152
OlegDsalo <87490184+OlegDsalo@users.noreply.github.com>
2021-09-29 18:57:24 +0300

    DEV-63 Create Skeleton For Backend

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express, {Application, Request, Response} from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+const PORT: number = 3000;
+
+app.get('/', cors(), (req: Request, res: Response) => {
+    res.send('<h1>Hello world</h1>');
+    res.end();
+});
+
+app.listen(PORT, () => console.log(`listening port ${PORT}`));
